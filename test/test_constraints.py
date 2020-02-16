ff64f60c4b16f7bcbacb4b1809673f1942e9f473
Douglas G. Moore <doug@dglmoore.com>
2020-02-15 19:59:18 -0700

    :hammer: Explicitly import all classes from constraints

--- test/test_constraints.py
+++ test/test_constraints.py
@@ -1,10 +1,13 @@
 import networkx as nx
 import randomneet
-import randomneet.constraints as rc
 import unittest
 
 from neet.boolean import LogicNetwork
 from neet.boolean.examples import s_pombe, myeloid
+from randomneet.constraints import AbstractConstraint, TopologicalConstraint, DynamicalConstraint, \
+    HasExternalNodes, IsConnected, IsIrreducible, \
+    HasCanalizingNodes, GenericTopological, GenericDynamical, \
+    ConstraintError
 
 
 class TestConstraints(unittest.TestCase):
@@ -30,33 +33,33 @@ class TestConstraints(unittest.TestCase):
         """
         The AbstractConstraint should be an abstract object
         """
-        self.assertTrue(issubclass(rc.AbstractConstraint, object))
+        self.assertTrue(issubclass(AbstractConstraint, object))
         with self.assertRaises(TypeError):
-            rc.AbstractConstraint()  # type: ignore
+            AbstractConstraint()  # type: ignore
 
     def test_topological_constraint(self):
         """
         The TopologicalConstraint should be an abstract subclass of
         AbstractConstraint
         """
-        self.assertTrue(issubclass(rc.TopologicalConstraint, rc.AbstractConstraint))
+        self.assertTrue(issubclass(TopologicalConstraint, AbstractConstraint))
         with self.assertRaises(TypeError):
-            rc.TopologicalConstraint()  # type: ignore
+            TopologicalConstraint()  # type: ignore
 
     def test_dynamical_constraint(self):
         """
         The DynamicalConstraint should be an abstract subclass of
         AbstractConstraint
         """
-        self.assertTrue(issubclass(rc.DynamicalConstraint, rc.AbstractConstraint))
+        self.assertTrue(issubclass(DynamicalConstraint, AbstractConstraint))
         with self.assertRaises(TypeError):
-            rc.DynamicalConstraint()  # type: ignore
+            DynamicalConstraint()  # type: ignore
 
     def test_has_external_nodes_is_topological(self):
         """
         The HasExternalNodes constraint is a TopologicalConstraint
         """
-        self.assertTrue(issubclass(rc.HasExternalNodes, rc.TopologicalConstraint))
+        self.assertTrue(issubclass(HasExternalNodes, TopologicalConstraint))
 
     def test_has_external_nodes_invalid_init(self):
         """
@@ -64,9 +67,9 @@ class TestConstraints(unittest.TestCase):
         initialization parameters.
         """
         with self.assertRaises(ValueError):
-            rc.HasExternalNodes(-1)
+            HasExternalNodes(-1)
         with self.assertRaises(TypeError):
-            rc.HasExternalNodes(nx.Graph())
+            HasExternalNodes(nx.Graph())
 
     def test_has_external_nodes_counts(self):
         """
@@ -74,21 +77,21 @@ class TestConstraints(unittest.TestCase):
         directed graph.
         """
         g = nx.DiGraph([(0, 1), (1, 2), (2, 1), (3, 1), (4, 5), (6, 6)])
-        constraint = rc.HasExternalNodes(g)
+        constraint = HasExternalNodes(g)
         self.assertEqual(constraint.num_external, 3)
 
     def test_has_external_nodes_saves_target(self):
         """
         HasExternalNodes properly stores the desired number of external edges.
         """
-        self.assertEqual(rc.HasExternalNodes(7).num_external, 7)
+        self.assertEqual(HasExternalNodes(7).num_external, 7)
 
     def test_has_external_nodes_raises(self):
         """
         HasExternalNodes.satisfies raises an error if the provided argument is
         not a directed graph.
         """
-        constraint = rc.HasExternalNodes(3)
+        constraint = HasExternalNodes(3)
         with self.assertRaises(TypeError):
             constraint.satisfies(3)
         with self.assertRaises(TypeError):
@@ -101,12 +104,12 @@ class TestConstraints(unittest.TestCase):
         """
         g = nx.DiGraph([(0, 1), (1, 2), (2, 1), (3, 1), (4, 5), (6, 6)])
 
-        constraint = rc.HasExternalNodes(3)
+        constraint = HasExternalNodes(3)
         self.assertFalse(constraint.satisfies(nx.DiGraph()))
         self.assertFalse(constraint.satisfies(nx.DiGraph([(0, 1), (1, 2), (2, 1), (3, 2)])))
         self.assertTrue(constraint.satisfies(g))
 
-        constraint = rc.HasExternalNodes(g)
+        constraint = HasExternalNodes(g)
         self.assertFalse(constraint.satisfies(nx.DiGraph()))
         self.assertFalse(constraint.satisfies(nx.DiGraph([(0, 1), (1, 2), (2, 1), (3, 2)])))
         self.assertTrue(constraint.satisfies(g))
@@ -116,14 +119,14 @@ class TestConstraints(unittest.TestCase):
         """
         The IsConnected constraint is a TopologicalConstraint
         """
-        self.assertTrue(issubclass(rc.IsConnected, rc.TopologicalConstraint))
+        self.assertTrue(issubclass(IsConnected, TopologicalConstraint))
 
     def test_is_connected_raises(self):
         """
         IsConnected.satisfies raises an error if the provided argument is not a
         directed graph.
         """
-        constraint = rc.HasExternalNodes(3)
+        constraint = HasExternalNodes(3)
         with self.assertRaises(TypeError):
             constraint.satisfies(3)
         with self.assertRaises(TypeError):
@@ -134,8 +137,8 @@ class TestConstraints(unittest.TestCase):
         IsConnected.satisfies raises an error if the provided argument is the
         null graph.
         """
-        constraint = rc.IsConnected()
-        with self.assertRaises(rc.ConstraintError):
+        constraint = IsConnected()
+        with self.assertRaises(ConstraintError):
             constraint.satisfies(nx.DiGraph())
 
     def test_is_connected_satisfies(self):
@@ -143,7 +146,7 @@ class TestConstraints(unittest.TestCase):
         IsConnected.satisfies correctly identifies directed graphs that are
         weakly connected.
         """
-        constraint = rc.IsConnected()
+        constraint = IsConnected()
         self.assertTrue(constraint.satisfies(self.empty_graph(1)))
         self.assertTrue(constraint.satisfies(nx.DiGraph([(0, 0)])))
         self.assertFalse(constraint.satisfies(self.empty_graph(2)))
@@ -154,14 +157,14 @@ class TestConstraints(unittest.TestCase):
         """
         The IsIrreducible constraint is a DynamicalConstraint.
         """
-        self.assertTrue(issubclass(rc.IsIrreducible, rc.DynamicalConstraint))
+        self.assertTrue(issubclass(IsIrreducible, DynamicalConstraint))
 
     def test_is_irreducible_raises(self):
         """
         IsIrreducible.satisfies raises an error if the argument is not a Neet
         network.
         """
-        constraint = rc.IsIrreducible()
+        constraint = IsIrreducible()
         with self.assertRaises(TypeError):
             constraint.satisfies(nx.DiGraph())
         with self.assertRaises(TypeError):
@@ -171,7 +174,7 @@ class TestConstraints(unittest.TestCase):
         """
         IsIrreducible.satisfies is not implemented for non-LogicNetworks
         """
-        constraint = rc.IsIrreducible()
+        constraint = IsIrreducible()
         with self.assertRaises(NotImplementedError):
             constraint.satisfies(s_pombe)
 
@@ -180,7 +183,7 @@ class TestConstraints(unittest.TestCase):
         IsIrreducible.satisfies correctly identifies networks that are
         irreducible.
         """
-        constraint = rc.IsIrreducible()
+        constraint = IsIrreducible()
         self.assertTrue(constraint.satisfies(myeloid))
 
         reducible = LogicNetwork([((0,), {'0', '1'})])
@@ -209,7 +212,7 @@ class TestConstraints(unittest.TestCase):
         """
         The HasCanalizingNodes constraint is a DynamicalConstraint
         """
-        self.assertTrue(issubclass(rc.HasCanalizingNodes, rc.DynamicalConstraint))
+        self.assertTrue(issubclass(HasCanalizingNodes, DynamicalConstraint))
 
     def test_has_canalizing_nodes_invalid_init(self):
         """
@@ -217,19 +220,19 @@ class TestConstraints(unittest.TestCase):
         initialization parameters.
         """
         with self.assertRaises(ValueError):
-            rc.HasCanalizingNodes(-1)
+            HasCanalizingNodes(-1)
         with self.assertRaises(TypeError):
-            rc.HasCanalizingNodes(nx.Graph())
+            HasCanalizingNodes(nx.Graph())
 
     def test_has_canalizing_nodes_counts(self):
         """
         HasCanalizingNodes properly counts the number of canalizing nodes in a
         network.
         """
-        constraint = rc.HasCanalizingNodes(myeloid)
+        constraint = HasCanalizingNodes(myeloid)
         self.assertEqual(constraint.num_canalizing, 11)
 
-        constraint = rc.HasCanalizingNodes(s_pombe)
+        constraint = HasCanalizingNodes(s_pombe)
         self.assertEqual(constraint.num_canalizing, 5)
 
     def test_has_canalizing_nodes_saves_target(self):
@@ -237,14 +240,14 @@ class TestConstraints(unittest.TestCase):
         HasCanalizingNodes properly stores the desired number of external
         edges.
         """
-        self.assertEqual(rc.HasCanalizingNodes(7).num_canalizing, 7)
+        self.assertEqual(HasCanalizingNodes(7).num_canalizing, 7)
 
     def test_has_canalizing_nodes_raises(self):
         """
         HasCanalizingNodes.satisfies raises an error if the provided argument
         is not a neet network
         """
-        constraint = rc.HasCanalizingNodes(3)
+        constraint = HasCanalizingNodes(3)
         with self.assertRaises(TypeError):
             constraint.satisfies(3)
         with self.assertRaises(TypeError):
@@ -257,7 +260,7 @@ class TestConstraints(unittest.TestCase):
         HasCanalizingNodes.satsifies correctly identifies networks the desired
         number of canalizing nodes.
         """
-        constraint = rc.HasCanalizingNodes(myeloid)
+        constraint = HasCanalizingNodes(myeloid)
         self.assertTrue(constraint.satisfies(myeloid))
         self.assertFalse(constraint.satisfies(s_pombe))
 
@@ -265,7 +268,7 @@ class TestConstraints(unittest.TestCase):
         """
         Ensure that GenericTopological is a subclass of TopologicalConstraint.
         """
-        self.assertTrue(issubclass(rc.GenericTopological, rc.TopologicalConstraint))
+        self.assertTrue(issubclass(GenericTopological, TopologicalConstraint))
 
     def test_generic_topological_raises(self):
         """
@@ -273,16 +276,16 @@ class TestConstraints(unittest.TestCase):
         anything that is not callable.
         """
         with self.assertRaises(TypeError):
-            rc.GenericTopological(5)
+            GenericTopological(5)
         with self.assertRaises(TypeError):
-            rc.GenericTopological(rc.IsConnected())
+            GenericTopological(IsConnected())
 
     def test_generic_topological_satisfies_raises(self):
         """
         GenericTopological.satisfies raises a TypeError when its argument is
         not a directed graph.
         """
-        allpass = rc.GenericTopological(lambda g: True)
+        allpass = GenericTopological(lambda g: True)
         with self.assertRaises(TypeError):
             allpass.satisfies(nx.Graph())
         with self.assertRaises(TypeError):
@@ -292,15 +295,15 @@ class TestConstraints(unittest.TestCase):
         """
         GenericTopological.satisfies correctly checks graphs.
         """
-        allpass = rc.GenericTopological(lambda g: True)
+        allpass = GenericTopological(lambda g: True)
         self.assertTrue(allpass.satisfies(self.empty_graph()))
         self.assertTrue(allpass.satisfies(nx.DiGraph([(0, 1), (1, 2), (2, 0)])))
 
-        allfail = rc.GenericTopological(lambda g: False)
+        allfail = GenericTopological(lambda g: False)
         self.assertFalse(allfail.satisfies(self.empty_graph()))
         self.assertFalse(allfail.satisfies(nx.DiGraph([(0, 1), (1, 2), (2, 0)])))
 
-        twonodes = rc.GenericTopological(lambda g: len(g) == 2)
+        twonodes = GenericTopological(lambda g: len(g) == 2)
         self.assertFalse(twonodes.satisfies(self.empty_graph()))
         self.assertTrue(twonodes.satisfies(self.empty_graph(2)))
 
@@ -308,7 +311,7 @@ class TestConstraints(unittest.TestCase):
         """
         Ensure that GenericDynamical is a subclass of DynamicalConstraint.
         """
-        self.assertTrue(issubclass(rc.GenericDynamical, rc.DynamicalConstraint))
+        self.assertTrue(issubclass(GenericDynamical, DynamicalConstraint))
 
     def test_generic_dynamical_raises(self):
         """
@@ -316,16 +319,16 @@ class TestConstraints(unittest.TestCase):
         that is not callable.
         """
         with self.assertRaises(TypeError):
-            rc.GenericDynamical(5)
+            GenericDynamical(5)
         with self.assertRaises(TypeError):
-            rc.GenericDynamical(rc.IsIrreducible())
+            GenericDynamical(IsIrreducible())
 
     def test_generic_dynamical_satisfies_raises(self):
         """
         GenericDynamical.satisfies raises a TypeError when its argument is not
         a network.
         """
-        allpass = rc.GenericDynamical(lambda n: True)
+        allpass = GenericDynamical(lambda n: True)
         with self.assertRaises(TypeError):
             allpass.satisfies(nx.Graph())
         with self.assertRaises(TypeError):
@@ -335,14 +338,14 @@ class TestConstraints(unittest.TestCase):
         """
         GenericDynamical.satisfies correctly checks networks.
         """
-        allpass = rc.GenericDynamical(lambda n: True)
+        allpass = GenericDynamical(lambda n: True)
         self.assertTrue(allpass.satisfies(myeloid))
         self.assertTrue(allpass.satisfies(s_pombe))
 
-        allfail = rc.GenericDynamical(lambda g: False)
+        allfail = GenericDynamical(lambda g: False)
         self.assertFalse(allfail.satisfies(myeloid))
         self.assertFalse(allfail.satisfies(s_pombe))
 
-        ninenodes = rc.GenericDynamical(lambda g: g.size == 9)
+        ninenodes = GenericDynamical(lambda g: g.size == 9)
         self.assertTrue(ninenodes.satisfies(s_pombe))
         self.assertFalse(ninenodes.satisfies(myeloid))
