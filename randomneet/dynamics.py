21c614a684b035ca91d0c8a87b545607445e432c
Doug Moore <doug@dglmoore.com>
2020-02-17 14:37:53 -0700

    :hammer: Implement NetworkRandomizer.__init__

--- randomneet/dynamics.py
+++ randomneet/dynamics.py
@@ -1,5 +1,32 @@
 from .randomizer import AbstractRandomizer
+from .topology import TopologyRandomizer, FixedTopology
+from inspect import isclass
 
 
 class NetworkRandomizer(AbstractRandomizer):
-    pass
+    def __init__(self, network, trand=None, constraints=None, timeout=1000, **kwargs):
+        """
+        An abstract base class for all randomizers which implement dynamical
+        randomization.
+
+        :param network: a base network or graph
+        :type network: neet.Network or networkx.DiGraph
+        :param trand: how to randomize the topology (default: FixedTopology)
+        :type trand: instance or subclass of TopologyRandomizer, or None
+        :param constraints: constraints used for rejection testing
+        :type constraints: a sequence of AbstractConstraint instances
+        :param timeout: the number of attempts before rejection testing times
+                        out. If less than 1, the rejection testing will never
+                        time out.
+        """
+        super().__init__(network, constraints, timeout)
+        if trand is None:
+            trand = FixedTopology(self.graph)
+        elif isclass(trand) and issubclass(trand, TopologyRandomizer):
+            trand = trand(self.graph)
+        elif isinstance(trand, TopologyRandomizer):
+            pass
+        else:
+            raise TypeError('trand must be an instance or subclass of TopologyRandomizer')
+
+        self.trand = trand
--- test/test_dynamics.py
+++ test/test_dynamics.py
@@ -4,6 +4,12 @@ import unittest
 from neet.boolean.examples import s_pombe
 from randomneet.dynamics import NetworkRandomizer
 from randomneet.randomizer import AbstractRandomizer
+from randomneet.topology import FixedTopology, MeanDegree
+
+
+class MockNetworkRandomizer(NetworkRandomizer):
+    def _randomize(self):
+        return self.network
 
 
 class TestDynamicsRandomizer(unittest.TestCase):
@@ -24,3 +30,15 @@ class TestDynamicsRandomizer(unittest.TestCase):
         self.assertTrue(issubclass(NetworkRandomizer, AbstractRandomizer))
         with self.assertRaises(TypeError):
             NetworkRandomizer(s_pombe)  # type: ignore
+
+    def test_randomizer_trand(self):
+        """
+        NetworkRandomizer instantiation with TopologyRandomizers
+        """
+        self.assertIsInstance(MockNetworkRandomizer(s_pombe).trand, FixedTopology)
+        self.assertIsInstance(MockNetworkRandomizer(s_pombe, MeanDegree).trand, MeanDegree)
+        self.assertIsInstance(MockNetworkRandomizer(s_pombe, MeanDegree(s_pombe)).trand, MeanDegree)
+        with self.assertRaises(TypeError):
+            MockNetworkRandomizer(s_pombe, AbstractRandomizer)
+        with self.assertRaises(TypeError):
+            MockNetworkRandomizer(s_pombe, MockNetworkRandomizer(s_pombe))
