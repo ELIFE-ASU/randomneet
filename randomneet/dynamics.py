4b01b8adc98c2a88f5fdf58723457aaed3749c24
Doug Moore <doug@dglmoore.com>
2020-02-17 20:53:14 -0700

    :bug: Add everything back

--- randomneet/dynamics.py
+++ randomneet/dynamics.py
@@ -1,8 +1,15 @@
+import neet
+import numpy as np
+
+from abc import abstractmethod
 from .randomizer import AbstractRandomizer
+from .topology import TopologyRandomizer, FixedTopology
+from .constraints import DynamicalConstraint, TopologicalConstraint, GenericDynamical, ConstraintError
+from inspect import isclass
 
 
 class NetworkRandomizer(AbstractRandomizer):
-    def __init__(self, network, **kwargs):
+    def __init__(self, network, trand=None, constraints=None, timeout=1000, **kwargs):
         """
         An abstract base class for all randomizers which implement dynamical
         randomization.
@@ -17,7 +24,127 @@ class NetworkRandomizer(AbstractRandomizer):
                         out. If less than 1, the rejection testing will never
                         time out.
         """
-        super().__init__(network, **kwargs)
+        if trand is None:
+            trand = FixedTopology(network, timeout=timeout, **kwargs)
+        elif isclass(trand) and issubclass(trand, TopologyRandomizer):
+            trand = trand(network, timeout=timeout, **kwargs)
+        elif isinstance(trand, TopologyRandomizer):
+            pass
+        else:
+            raise TypeError('trand must be an instance or subclass of TopologyRandomizer')
+        self.trand = trand
+        super().__init__(network, constraints, timeout, **kwargs)
 
-    def _randomize(self):
-        return self.network
+    @property
+    def constraints(self):
+        return super().constraints
+
+    @constraints.setter
+    def constraints(self, constraints):
+        """
+        Set the randomizer's constraints.
+
+        :param constraints: the new constraints
+        :type constraints: a seq of AbstractConstraint instances
+        :raises TypeError: if any of the contraints are not an AbstractConstraint
+        """
+        if constraints is None:
+            constraints = []
+        elif not isinstance(constraints, list):
+            constraints = list(constraints)
+
+        tconstraints, dconstraints = [], []
+
+        for i, constraint in enumerate(constraints):
+            if isinstance(constraint, DynamicalConstraint):
+                dconstraints.append(constraint)
+            elif isinstance(constraint, TopologicalConstraint):
+                tconstraints.append(constraint)
+            elif callable(constraint):
+                dconstraints.append(GenericDynamical(constraint))
+            else:
+                msg = 'constraints must be callable, a DynamicalConstraint or TopologicalConstraint'
+                raise TypeError(msg)
+
+        self.trand.constraints = tconstraints
+        AbstractRandomizer.constraints.__set__(self, dconstraints)  # type: ignore
+
+    def add_constraint(self, constraint):
+        """
+        Append a constraint to the randomizer's list of constraints.
+
+        :param constraint: the new constraint
+        :type constraint: AbstractConstraint
+        :raises TypeError: if the constraint is not an AbstractConstraint
+        """
+        if isinstance(constraint, DynamicalConstraint):
+            super().add_constraint(constraint)
+        elif callable(constraint):
+            super().add_constraint(GenericDynamical(constraint))
+        elif isinstance(constraint, TopologicalConstraint):
+            self.trand.add_constraint(constraint)
+        else:
+            msg = 'constraints must be callable, a DynamicalConstraint or TopologicalConstraint'
+            raise TypeError(msg)
+
+    def random(self):
+        topology = self.trand.random()
+
+        loop = 0
+        while self.timeout <= 0 or loop < self.timeout:
+            net = self._randomize(topology)
+            if self._check_constraints(net):
+                return net
+            loop += 1
+        raise ConstraintError('failed to generate a network that statisfies all constraints')
+
+    def _randomize(self, topology):
+        table = []
+        for node in topology.nodes:
+            predecessors = tuple(topology.predecessors(node))
+            params = self._function_class_parameters(topology, node)
+            table.append((predecessors, self._random_function(**params)))
+        return neet.boolean.LogicNetwork(table)
+
+    def _random_function(self, k, p, **kwargs):
+        volume = 2**k
+        integer, decimal = divmod(p * volume, 1)
+        num_states = int(integer + np.random.choice(2, p=[1 - decimal, decimal]))
+        indices = np.random.choice(volume, num_states, replace=False)
+        return set('{0:0{1}b}'.format(index, k) for index in indices)
+
+    @abstractmethod
+    def _function_class_parameters(self, topology, node):
+        return {'topology': topology, 'node': node, 'k': topology.in_degree(node)}
+
+
+class UniformBias(NetworkRandomizer):
+    def __init__(self, network, p=0.5, **kwargs):
+        """
+        Generate random Boolean networks with the same bias on each non-external
+        node.
+        """
+        super().__init__(network, **kwargs)
+        self.p = p
+
+    def _function_class_parameters(self, topology, node):
+        params = super()._function_class_parameters(topology, node)
+        params.update({'p': self.p})
+        return params
+
+
+class MeanBias(UniformBias):
+    def __init__(self, network, **kwargs):
+        """
+        Generate random Boolean networks with the same mean bias (on average)
+        as the original network.
+        """
+        if not isinstance(network, neet.boolean.LogicNetwork):
+            raise NotImplementedError()
+        super().__init__(network, self._mean_bias(network), **kwargs)
+
+    def _mean_bias(self, network):
+        """
+        Get the mean bias of a network
+        """
+        return np.mean([float(len(row[1]) / 2**len(row[0])) for row in network.table])
--- test/test_dynamics.py
+++ test/test_dynamics.py
@@ -1,8 +1,47 @@
+import math
+import networkx as nx
 import randomneet
+import statistics
 import unittest
 
-from randomneet.dynamics import NetworkRandomizer
+from neet.boolean.examples import s_pombe, myeloid
+from randomneet.dynamics import NetworkRandomizer, UniformBias, MeanBias
+from randomneet.constraints import GenericDynamical, GenericTopological, ConstraintError
 from randomneet.randomizer import AbstractRandomizer
+from randomneet.topology import TopologyRandomizer, FixedTopology, MeanDegree
+from randomneet.constraints import IsConnected, IsIrreducible
+from itertools import islice
+
+
+class MockTopologyRandomizer(TopologyRandomizer):
+    """
+    A mock implementation of the AbstractRandomizer base class
+    """
+    counter = 0
+
+    def _randomize(self):
+        """
+        Generate successively larger empty graphs
+        """
+        self.counter += 1
+        return nx.DiGraph([(i, j) for i in range(self.counter) for j in range(self.counter)])
+
+
+class MockNetworkRandomizer(NetworkRandomizer):
+    p = 0.0
+
+    def _randomize(self, topology):
+        network = super()._randomize(topology)
+        self.p += 0.1
+        return network
+
+    def _function_class_parameters(self, topology, node):
+        """
+        Generate parameters with successively higher bias
+        """
+        d = super()._function_class_parameters(topology, node)
+        d.update({'k': topology.in_degree(node), 'p': self.p})
+        return d
 
 
 class TestNetworkRandomizer(unittest.TestCase):
@@ -21,3 +60,253 @@ class TestNetworkRandomizer(unittest.TestCase):
         NetworkRandomizer should be an abstract object
         """
         self.assertTrue(issubclass(NetworkRandomizer, AbstractRandomizer))
+        with self.assertRaises(TypeError):
+            NetworkRandomizer(s_pombe)  # type: ignore
+
+    def test_network_randomizer_trand(self):
+        """
+        NetworkRandomizer instantiation with TopologyRandomizers
+        """
+        self.assertIsInstance(MockNetworkRandomizer(s_pombe).trand, FixedTopology)
+        self.assertIsInstance(MockNetworkRandomizer(s_pombe, MeanDegree).trand, MeanDegree)
+        self.assertIsInstance(MockNetworkRandomizer(s_pombe, MeanDegree(s_pombe)).trand, MeanDegree)
+        with self.assertRaises(TypeError):
+            MockNetworkRandomizer(s_pombe, AbstractRandomizer)
+        with self.assertRaises(TypeError):
+            MockNetworkRandomizer(s_pombe, MockNetworkRandomizer(s_pombe))
+
+    def test_network_randomizer_propogates_timeout(self):
+        """
+        The timeout passed to the NetworkRandomizer initializer is propagated
+        to trand when it's a class.
+        """
+        rand = MockNetworkRandomizer(s_pombe, timeout=5)
+        self.assertEqual(rand.timeout, 5)
+        self.assertEqual(rand.trand.timeout, 5)
+
+        rand = MockNetworkRandomizer(s_pombe, trand=MeanDegree, timeout=5)
+        self.assertEqual(rand.timeout, 5)
+        self.assertEqual(rand.trand.timeout, 5)
+
+    def test_network_randomizer_constraints(self):
+        """
+        Ensure that the randomizer correctly instantiates the constraints
+        """
+        c1, c2, c3 = IsIrreducible(), (lambda _: False), IsConnected()
+
+        rand = MockNetworkRandomizer(s_pombe, constraints=[c1])
+        self.assertEqual(rand.network, s_pombe)
+        self.assertTrue(nx.is_isomorphic(rand.graph, s_pombe.network_graph()))
+        self.assertEqual(rand.timeout, 1000)
+        self.assertEqual(rand.constraints, [c1])
+        self.assertEqual(rand.trand.constraints, [])
+
+        rand = MockNetworkRandomizer(s_pombe, constraints=[c2])
+        self.assertEqual(rand.network, s_pombe)
+        self.assertTrue(nx.is_isomorphic(rand.graph, s_pombe.network_graph()))
+        self.assertEqual(rand.timeout, 1000)
+        self.assertEqual(len(rand.constraints), 1)
+        self.assertIsInstance(rand.constraints[0], GenericDynamical)
+        self.assertEqual(rand.constraints[0].test, c2)
+        self.assertEqual(rand.trand.constraints, [])
+
+        rand = MockNetworkRandomizer(s_pombe, constraints=[c3])
+        self.assertEqual(rand.network, s_pombe)
+        self.assertTrue(nx.is_isomorphic(rand.graph, s_pombe.network_graph()))
+        self.assertEqual(rand.timeout, 1000)
+        self.assertEqual(rand.constraints, [])
+        self.assertEqual(rand.trand.constraints, [c3])
+
+        rand = MockNetworkRandomizer(s_pombe, constraints=[c1, c3, c2])
+        self.assertEqual(rand.network, s_pombe)
+        self.assertTrue(nx.is_isomorphic(rand.graph, s_pombe.network_graph()))
+        self.assertEqual(rand.timeout, 1000)
+        self.assertEqual(len(rand.constraints), 2)
+        self.assertEqual(rand.constraints[0], c1)
+        self.assertIsInstance(rand.constraints[1], GenericDynamical)
+        self.assertEqual(rand.constraints[1].test, c2)
+        self.assertEqual(rand.trand.constraints, [c3])
+
+        with self.assertRaises(TypeError):
+            MockNetworkRandomizer(s_pombe, constraints=[5])
+
+    def test_network_randomizer_set_constraints(self):
+        """
+        Ensure that the randomizer correctly sets the constraints
+        """
+        c1, c2, c3 = IsIrreducible(), (lambda _: False), IsConnected()
+
+        rand = MockNetworkRandomizer(s_pombe)
+
+        rand.constraints = [c1]
+        self.assertEqual(rand.constraints, [c1])
+        self.assertEqual(rand.trand.constraints, [])
+
+        rand.constraints = [c2]
+        self.assertEqual(len(rand.constraints), 1)
+        self.assertIsInstance(rand.constraints[0], GenericDynamical)
+        self.assertEqual(rand.constraints[0].test, c2)
+        self.assertEqual(rand.trand.constraints, [])
+
+        rand.constraints = [c3]
+        self.assertEqual(rand.constraints, [])
+        self.assertEqual(rand.trand.constraints, [c3])
+
+        rand.constraints = [c1, c2, c3]
+        self.assertEqual(len(rand.constraints), 2)
+        self.assertEqual(rand.constraints[0], c1)
+        self.assertIsInstance(rand.constraints[1], GenericDynamical)
+        self.assertEqual(rand.constraints[1].test, c2)
+        self.assertEqual(rand.trand.constraints, [c3])
+
+        rand.constraints = {c1}
+        self.assertEqual(rand.constraints, [c1])
+        self.assertEqual(rand.trand.constraints, [])
+
+        rand.constraints = (c3,)
+        self.assertEqual(rand.constraints, [])
+        self.assertEqual(rand.trand.constraints, [c3])
+
+        with self.assertRaises(TypeError):
+            rand.constraints = [5]
+
+    def test_network_randomizer_add_constraints(self):
+        """
+        Ensure that the randomizer correctly adds the constraints
+        """
+        c1, c2, c3 = IsIrreducible(), (lambda _: False), IsConnected()
+
+        rand = MockNetworkRandomizer(s_pombe)
+
+        rand.add_constraint(c1)
+        self.assertEqual(rand.constraints, [c1])
+        self.assertEqual(rand.trand.constraints, [])
+
+        rand.add_constraint(c2)
+        self.assertEqual(len(rand.constraints), 2)
+        self.assertEqual(rand.constraints[0], c1)
+        self.assertIsInstance(rand.constraints[1], GenericDynamical)
+        self.assertEqual(rand.constraints[1].test, c2)
+        self.assertEqual(rand.trand.constraints, [])
+
+        rand.add_constraint(c3)
+        self.assertEqual(len(rand.constraints), 2)
+        self.assertEqual(rand.constraints[0], c1)
+        self.assertIsInstance(rand.constraints[1], GenericDynamical)
+        self.assertEqual(rand.constraints[1].test, c2)
+        self.assertEqual(rand.trand.constraints, [c3])
+
+        with self.assertRaises(TypeError):
+            rand.add_constraint(5)
+
+    def test_random_timeout(self):
+        """
+        Ensure the randomizer times out properly
+        """
+        def allfail(_):
+            return False
+
+        tc, dc = GenericTopological(allfail), GenericDynamical(allfail)
+        rand = MockNetworkRandomizer(s_pombe, MockTopologyRandomizer, constraints=[tc], timeout=10)
+        with self.assertRaises(ConstraintError):
+            rand.random()
+
+        rand = MockNetworkRandomizer(s_pombe, MockTopologyRandomizer, constraints=[dc], timeout=10)
+        with self.assertRaises(ConstraintError):
+            rand.random()
+
+    def test_random(self):
+        def bias(network):
+            return [float(len(row[1]) / 2**len(row[0])) for row in network.table]
+
+        rand = MockNetworkRandomizer(s_pombe, MockTopologyRandomizer, timeout=20)
+        rand.add_constraint(lambda n: bias(n) == [1.0] * n.size)
+        rand.add_constraint(GenericTopological(lambda g: len(g) == 10))
+
+        network = rand.random()
+        self.assertEqual(len(network.network_graph()), 10)
+
+
+class TestUniformBias(unittest.TestCase):
+    """
+    Unit tests for the UniformBias randomizer
+    """
+
+    def bias(self, network):
+        """
+        Get the bias of each node in the network
+        """
+        return [float(len(row[1]) / 2**len(row[0])) for row in network.table]
+
+    def take(self, n, iter):
+        """
+        Take the first ``n`` elements from ``iter``.
+        """
+        return islice(iter, n)
+
+    def test_uniform_bias(self):
+        """
+        Ensure that UniformBias is a NetworkRandomizer
+        """
+        self.assertIsInstance(UniformBias(myeloid), NetworkRandomizer)
+
+    def test_default_bias(self):
+        """
+        Ensure the generated networks have a bias of 0.5 by default
+        """
+        rand = UniformBias(s_pombe)
+        expect = [0.0 if k == 0 else 0.5 for k in dict(s_pombe.network_graph().in_degree).values()]
+        got = list(map(self.bias, self.take(10, rand)))
+        self.assertEqual(got, [expect] * len(got))
+
+    def test_other_bias(self):
+        """
+        Ensure the generated networks have the specified
+        """
+        p = 0.3
+
+        rand = UniformBias(s_pombe, p)
+        indegree = dict(s_pombe.network_graph().in_degree).values()
+        high = [0.0 if k == 0 else math.ceil(p * 2**k) / 2**k for k in indegree]
+        low = [0.0 if k == 0 else math.floor(p * 2**k) / 2**k for k in indegree]
+        for net in self.take(10, rand):
+            got = self.bias(net)
+            for g, l, h in zip(got, low, high):
+                try:
+                    self.assertTrue(g == l or g == h)
+                except Exception as e:
+                    raise Exception(got, low, high) from e
+
+
+class TestMeanBias(unittest.TestCase):
+    """
+    Unit tests for the UniformBias randomizer
+    """
+
+    def take(self, n, iter):
+        """
+        Take the first ``n`` elements from ``iter``.
+        """
+        return islice(iter, n)
+
+    def test_uniform_bias(self):
+        """
+        Ensure that MeanBias is a UniformBias
+        """
+        self.assertIsInstance(MeanBias(myeloid), UniformBias)
+
+    def test_unimplemented(self):
+        """
+        MeanBias is currently only implemented for logic network
+        """
+        with self.assertRaises(NotImplementedError):
+            MeanBias(s_pombe)
+
+    def test_bias(self):
+        """
+        Ensure the generated networks have a the same mean bias as the original
+        network (on average).
+        """
+        rand = MeanBias(myeloid)
+        got = statistics.mean(map(rand._mean_bias, self.take(100, rand)))
+        self.assertAlmostEqual(got, rand.p, places=2)
