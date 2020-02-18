54e6961c629b9a47ae37182ad16fd33c5b6164ae
Doug Moore <doug@dglmoore.com>
2020-02-17 20:40:58 -0700

    :fire: Remove __init__

--- randomneet/dynamics.py
+++ randomneet/dynamics.py
@@ -2,22 +2,5 @@ from .randomizer import AbstractRandomizer
 
 
 class NetworkRandomizer(AbstractRandomizer):
-    def __init__(self, network, **kwargs):
-        """
-        An abstract base class for all randomizers which implement dynamical
-        randomization.
-
-        :param network: a base network or graph
-        :type network: neet.Network or networkx.DiGraph
-        :param trand: how to randomize the topology (default: FixedTopology)
-        :type trand: instance or subclass of TopologyRandomizer, or None
-        :param constraints: constraints used for rejection testing
-        :type constraints: a sequence of AbstractConstraint instances
-        :param timeout: the number of attempts before rejection testing times
-                        out. If less than 1, the rejection testing will never
-                        time out.
-        """
-        super().__init__(network, **kwargs)
-
     def _randomize(self):
         return self.network
